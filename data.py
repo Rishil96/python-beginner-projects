9532a5b0c907e7a92aad30e3023812ff2f56a315
Rishil Ramesh <rishil.ramesh@outlook.com>
2024-09-15 13:35:49 +0530

    Add : Initial project requirements and data to build coffee machine

---
+++ data.py
@@ -0,0 +1,33 @@
+# Machine start ingredients
+ingredients = {
+    "water": 300,
+    "milk": 200,
+    "coffee": 100
+}
+
+# Coin conversation in cents
+coins = {
+    "penny": 1,
+    "nickel": 5,
+    "dime": 10,
+    "quarter": 25
+}
+
+# Coffee requirements
+coffee_requirements = {
+    "espresso": {
+        "water": 50,
+        "coffee": 18,
+        "milk": 0
+    },
+    "latte": {
+        "water": 200,
+        "coffee": 24,
+        "milk": 150
+    },
+    "cappuccino": {
+        "water": 250,
+        "coffee": 24,
+        "milk": 100
+    }
+}
