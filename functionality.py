46a6e16f521c579e1a4e767cd216a5be515408b8
Rishil Ramesh <rishil.ramesh@outlook.com>
2024-09-15 15:00:46 +0530

    Add : Functions to check if correct amount is paid, make coffee and function to process the whole coffee buying scenario

--- functionality.py
+++ functionality.py
@@ -1,5 +1,5 @@
 # This module contains the functions of each functionality available in the coffee machine
-from data import coffee_requirements, coins
+from data import coffee_requirements, coins, coffee_costs
 
 
 # Print report
@@ -17,13 +17,14 @@ def is_enough_resources(coffee: str, resources_available: dict) -> bool:
     # Check if all the resources are enough to make coffee
     for ingredient in coffee_requirements[coffee]:
         if not resources_available.get(ingredient, 0) >= coffee_requirements[coffee][ingredient]:
-            print(f"Sorry, there isn't enough {ingredient} available at the moment. Please come back later.")
+            print(f"There isn't enough {ingredient} available at the moment")
             return False
     return True
 
 
 # Get payment from the user
-def get_payment() -> float:
+def get_payment(coffee: str) -> float:
+    print(f"Please pay ${coffee_costs[coffee]} for {coffee}.")
     print("Please insert coins.")
     quarters = int(input("How many quarters?: "))
     dimes = int(input("How many dimes?: "))
@@ -33,3 +34,38 @@ def get_payment() -> float:
                     nickels * coins["nickel"] + pennies * coins["penny"])
     print(f"Total amount paid: ${total_amount}")
     return total_amount
+
+
+# Check if user has paid the correct amount
+def is_coffee_paid(coffee: str, paid_amount: float) -> bool:
+    is_paid = paid_amount >= coffee_costs[coffee]
+    if is_paid:
+        print(f"Here is your change while we make your coffee: ${round(paid_amount - coffee_costs[coffee], 2)}")
+    return is_paid
+
+
+# Make coffee by using resources
+def make_coffee(coffee: str, available_resources: dict) -> None:
+    # Use resources from the machine
+    for resource in coffee_requirements[coffee]:
+        available_resources[resource] -= coffee_requirements[coffee][resource]
+    # Add the paid amount to the machine
+    available_resources["money"] += coffee_costs[coffee]
+    print(f"Here is your delicious {coffee}. Please enjoy☕!")
+
+
+# Process coffee: all steps that goes into getting coffee
+def process_coffee(coffee: str, available_resources: dict) -> None:
+    # Check if resources are available to make the requested coffee
+    if is_enough_resources(coffee, available_resources):
+        # Get payment from the user
+        payment = get_payment(coffee)
+        # Check if correct amount is paid
+        if is_coffee_paid(coffee, payment):
+            # Make coffee and return the change
+            make_coffee(coffee, available_resources)
+        else:
+            print(f"You have not paid the correct amount for {coffee}. Money refunded.")
+    else:
+        print("Apologies for the inconvenience, we will stock up the ingredients soon.")
+        print("Please come back later.")
--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 # Coffee Machine
 import art
 import data
-import functionality
+from functionality import print_report, process_coffee
 
 # Load up resources in the machine
 available_resources = data.starting_resources
@@ -12,15 +12,28 @@ print("Starting coffee machine...")
 print(art.logo)
 
 while is_on:
+    # Steps to make coffee
+    # 1. Check if enough resources are available to make espresso
+    # 2. Get payment in coins from user
+    # 3. Check if payment is correct
+    # 4. Make coffee for the customer
+    print("************************************************")
     user_input = input("What would you like? (espresso/latte/cappuccino): ").lower()
 
     # Print Report
     if user_input == "report":
-        functionality.print_report(available_resources)
-    # Make Espresso
-    # Make Latte
-    # Make Cappuccino
+        print_report(available_resources)
+
+    # Make Espresso, Latte, Cappuccino
+    elif user_input in ["espresso", "latte", "cappuccino"]:
+        process_coffee(user_input, available_resources)
+
     # Turn off machine
     elif user_input == "off":
         print("Turning off the coffee machine...")
+        print("Coffee is life. Please visit again!")
         is_on = False
+
+    # Unknown input
+    else:
+        print("Could not understand your request. Please try again!")
