ca5e9e55abce17dd16440cd702b09cdf56863ea9
Rishil Ramesh <rishil.ramesh@outlook.com>
2024-09-15 15:06:55 +0530

    Change : Coffee Machine logic into a single function in main file and change in logo

--- main.py
+++ main.py
@@ -3,37 +3,50 @@ import art
 import data
 from functionality import print_report, process_coffee
 
-# Load up resources in the machine
-available_resources = data.starting_resources
 
-# Start coffee machine
-is_on = True
-print("Starting coffee machine...")
-print(art.logo)
+def start_coffee_machine():
+    # Load up resources in the machine
+    available_resources = data.starting_resources
 
-while is_on:
-    # Steps to make coffee
-    # 1. Check if enough resources are available to make espresso
-    # 2. Get payment in coins from user
-    # 3. Check if payment is correct
-    # 4. Make coffee for the customer
-    print("************************************************")
-    user_input = input("What would you like? (espresso/latte/cappuccino): ").lower()
+    # Start coffee machine
+    is_on = True
+    print("Starting PyCafe machine...")
+    print(art.logo)
 
-    # Print Report
-    if user_input == "report":
-        print_report(available_resources)
+    while is_on:
+        # Steps to make coffee
+        # 1. Check if enough resources are available to make espresso
+        # 2. Get payment in coins from user
+        # 3. Check if payment is correct
+        # 4. Make coffee for the customer
 
-    # Make Espresso, Latte, Cappuccino
-    elif user_input in ["espresso", "latte", "cappuccino"]:
-        process_coffee(user_input, available_resources)
+        print("************************************************")
+        user_input = input("What would you like? (espresso/latte/cappuccino): ").lower()
 
-    # Turn off machine
-    elif user_input == "off":
-        print("Turning off the coffee machine...")
-        print("Coffee is life. Please visit again!")
-        is_on = False
+        # Print Report
+        if user_input == "report":
+            print_report(available_resources)
 
-    # Unknown input
+        # Make Espresso, Latte, Cappuccino
+        elif user_input in ["espresso", "latte", "cappuccino"]:
+            process_coffee(user_input, available_resources)
+
+        # Turn off machine
+        elif user_input == "off":
+            print("Turning off the coffee machine...")
+            is_on = False
+
+        # Unknown input
+        else:
+            print("Could not understand your request. Please try again!")
+
+    # Restart machine?
+    restart = input("Would you like to restart the PyCafe Machine? Type 'y' or 'n': ").lower()
+    if restart == 'y':
+        start_coffee_machine()
     else:
-        print("Could not understand your request. Please try again!")
+        print("Coffee is life. Please visit PyCafe again!")
+
+
+if __name__ == "__main__":
+    start_coffee_machine()
