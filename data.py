2806d8452a60fa80ddf6cdd23281364fd15b84c8
Rishil Ramesh <rishil.ramesh@outlook.com>
2024-09-17 12:51:25 +0530

    Add : Questions for quiz

---
+++ data.py
@@ -0,0 +1,48 @@
+question_data = [
+    {"question": "A slug's blood is green.", "correct_answer": "True"},
+    {"question": "The loudest animal is the African Elephant.", "correct_answer": "False"},
+    {"question": "Approximately one quarter of human bones are in the feet.", "correct_answer": "True"},
+    {"question": "The total surface area of a human lungs is the size of a football pitch.", "correct_answer": "True"},
+    {"question": "In West Virginia, USA, if you accidentally hit an animal with your car, you are free to take it "
+                 "home to eat.",
+     "correct_answer": "True"},
+    {"question": "In London, UK, if you happen to die in the House of Parliament, you are entitled to a state funeral.",
+     "correct_answer": "False"},
+    {"question": "It is illegal to pee in the Ocean in Portugal.", "correct_answer": "True"},
+    {"question": "You can lead a cow down stairs but not up stairs.", "correct_answer": "False"},
+    {"question": "Google was originally called 'Backrub'.", "correct_answer": "True"},
+    {"question": "Buzz Aldrin's mother's maiden name was 'Moon'.", "correct_answer": "True"},
+    {"question": "No piece of square dry paper can be folded in half more than 7 times.", "correct_answer": "False"},
+    {"question": "A few ounces of chocolate can to kill a small dog.", "correct_answer": "True"}
+]
+
+question_computers = [
+    {"category": "Science: Computers", "type": "boolean", "difficulty": "easy",
+     "question": "Linus Torvalds created Linux and Git.", "correct_answer": "True", "incorrect_answers": ["False"]},
+    {"category": "Science: Computers", "type": "boolean", "difficulty": "easy",
+     "question": "The programming language &quot;Python&quot; is based off a modified version of "
+                 "&quot;JavaScript&quot;.",
+     "correct_answer": "False", "incorrect_answers": ["True"]},
+    {"category": "Science: Computers", "type": "boolean", "difficulty": "easy",
+     "question": "Pointers were not used in the original C programming language; they were added later on in C++.",
+     "correct_answer": "False", "incorrect_answers": ["True"]},
+    {"category": "Science: Computers", "type": "boolean", "difficulty": "easy",
+     "question": "Ada Lovelace is often considered the first computer programmer.", "correct_answer": "True",
+     "incorrect_answers": ["False"]}, {"category": "Science: Computers", "type": "boolean", "difficulty": "easy",
+                                       "question": "&quot;HTML&quot; stands for Hypertext Markup Language.",
+                                       "correct_answer": "True", "incorrect_answers": ["False"]},
+    {"category": "Science: Computers", "type": "boolean", "difficulty": "easy",
+     "question": "In most programming languages, the operator ++ is equivalent to the statement &quot;+= 1&quot;.",
+     "correct_answer": "True", "incorrect_answers": ["False"]},
+    {"category": "Science: Computers", "type": "boolean", "difficulty": "easy",
+     "question": "Time on Computers is measured via the EPOX System.", "correct_answer": "False",
+     "incorrect_answers": ["True"]}, {"category": "Science: Computers", "type": "boolean", "difficulty": "easy",
+                                      "question": "The NVidia GTX 1080 gets its name because it can only render at a "
+                                                  "1920x1080 screen resolution.",
+                                      "correct_answer": "False", "incorrect_answers": ["True"]},
+    {"category": "Science: Computers", "type": "boolean", "difficulty": "easy",
+     "question": "Linux was first created as an alternative to Windows XP.", "correct_answer": "False",
+     "incorrect_answers": ["True"]}, {"category": "Science: Computers", "type": "boolean", "difficulty": "easy",
+                                      "question": "The Python programming language gets its name from the British "
+                                                  "comedy group &quot;Monty Python.&quot;",
+                                      "correct_answer": "True", "incorrect_answers": ["False"]}]
