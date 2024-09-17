67ea24542de9bfce9aaabf22c001d18abf6dd288
Rishil Ramesh <rishil.ramesh@outlook.com>
2024-09-17 13:35:56 +0530

    Change: Quiz logic into a function and add replay functionality

--- main.py
+++ main.py
@@ -2,23 +2,39 @@
 from data import question_data, question_computers
 from question_model import Question
 from quiz_brain import QuizBrain
+from art import logo
 
-# Load questions
-question_bank = []
 
-# Ask user to choose the quiz genre
-quiz_genre = input("Which quiz would you like to take? Type 'general' or 'computer': ").lower()
-questions_list = question_computers if quiz_genre == "computer" else question_data
+def play_quiz():
+    # Load questions
+    question_bank = []
 
-for question in questions_list:
-    new_question = Question(text=question.get("question", ""), answer=question.get("correct_answer", ""))
-    question_bank.append(new_question)
+    # Ask user to choose the quiz genre
+    quiz_genre = input("Which quiz would you like to take? Type 'general' or 'computer': ").lower()
+    questions_list = question_computers if quiz_genre == "computer" else question_data
 
-# Initialize QuizBrain
-quiz = QuizBrain(question_list=question_bank)
+    for question in questions_list:
+        new_question = Question(text=question.get("question", ""), answer=question.get("correct_answer", ""))
+        question_bank.append(new_question)
 
-# Start quiz and run till we have questions left
-while quiz.still_has_questions():
-    quiz.next_question()
+    # Initialize QuizBrain
+    quiz = QuizBrain(question_list=question_bank)
 
-print(f"Your final score is {quiz.score}/{quiz.question_number}!")
+    print(logo)
+
+    # Start quiz and run till we have questions left
+    while quiz.still_has_questions():
+        quiz.next_question()
+
+    # Print final score
+    print(f"Your final score is {quiz.score}/{quiz.question_number}!")
+
+    restart_quiz = input("\nWould you like to take the quiz again? Type 'y' or 'n': ").lower()
+    if restart_quiz == 'y':
+        play_quiz()
+    else:
+        print("Thanks for quizzing!")
+
+
+if __name__ == "__main__":
+    play_quiz()
